3434d33216b87b677afb8a8aa71f6d9148a448e7
Jack Murray <jack@murray.software>
2024-03-02 05:04:22 +0000

    start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-/home/jack/ccgc/env/uwsgi --chdir=/home/jack/ccgc \
+/home/jack/ccgc/env/bin/uwsgi --chdir=/home/jack/ccgc \
     --module=ccgc.wsgi:application \
     --env DJANGO_SETTINGS_MODULE=ccgc.settings \
     --master --pidfile=/tmp/ccgc-master.pid \
-    --socket=127.0.0.1:5000 \      # can also be a file
-    --processes=5 \                 # number of worker processes
-    --vacuum \                      # clear environment on exit
+    --socket=127.0.0.1:5000 \
+    --processes=5 \
+    --vacuum \
     --home=/home/jack/ccgc/env
